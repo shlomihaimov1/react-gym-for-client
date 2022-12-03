98e7179920d4b18d97a2ea22f1b54a5f858191d9
Your Name <you@example.com>
2022-12-04 02:05:35 +0500

    code update

--- src/New/components/Department.js
+++ src/New/components/Department.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../style/department.scss'
+import img from '../images/crutches.png'
 import { RiLightbulbFlashLine } from 'react-icons/ri';
 function Department() {
   return (
@@ -24,9 +25,21 @@ Lorem Ipsum the industry's standard dummy text.</p>
    <div className='dep'><div className='dep__circle'><RiLightbulbFlashLine className='dep__icon' size={50}/></div>
    <h2>Qualified Doctors</h2>
    </div>
-
-
    </div>
+
+{/* picture with text */}
+   <div className='dep__pic__text container'>
+   <div className='dep_pic'><img src={img}/></div>
+    <div className='dep__text'><h1>Welcome to our Crutches <span> Crutches</span></h1> <p>
+Welcome to our Crutches
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nemo libero debitis vitae sapiente quos.
+</p>
+<span>Oillum abrem deleniti adipisci suscipit dignissimos. remaining essentially unchanged. It was popularised in the with the . Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+<button></button>
+</div>
+   </div>
+
+
   </div>
   )
 }
