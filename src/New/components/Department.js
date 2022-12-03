482b3bb21fe47d183f129583bd2b9e008529594d
Your Name <you@example.com>
2022-12-04 01:49:23 +0500

    code update

--- src/New/components/Department.js
+++ src/New/components/Department.js
@@ -8,27 +8,19 @@ function Department() {
      <div className='line'></div>
      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br></br>
 Lorem Ipsum the industry's standard dummy text.</p>
-    <div className='deps container'>
+    <div className='deps scrollmenu container'>
    <div className='dep'><div className='dep__circle'><RiLightbulbFlashLine className='dep__icon' size={50}/></div>
    <h2>Qualified Doctors</h2>
    </div>
-
    <div className='dep'><div className='dep__circle'><RiLightbulbFlashLine className='dep__icon' size={50}/></div>
    <h2>Qualified Doctors</h2>
    </div>
-
-
    <div className='dep'><div className='dep__circle'><RiLightbulbFlashLine className='dep__icon' size={50}/></div>
    <h2>Qualified Doctors</h2>
    </div>
-
-
    <div className='dep'><div className='dep__circle'><RiLightbulbFlashLine className='dep__icon' size={50}/></div>
    <h2>Qualified Doctors</h2>
    </div>
-
-
-
    <div className='dep'><div className='dep__circle'><RiLightbulbFlashLine className='dep__icon' size={50}/></div>
    <h2>Qualified Doctors</h2>
    </div>
